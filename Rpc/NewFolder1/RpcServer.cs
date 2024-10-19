22a63dfde1da78f04d05ac2651399095cb8e62fc
Egg <Egg@LAPTOP-S5N0L13S>
2024-10-19 17:34:27 +0800

    Add some test

---
+++ Rpc/NewFolder1/RpcServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpc.Rpc
+{
+	public class RpcServer<ServerContract>
+	{
+        public RpcServer(ServerContract serverHandle)
+        {
+            
+        }
+    }
+}
---
+++ RpcTests/MessagePackTest/MessagePackTest.cs
@@ -0,0 +1,57 @@
+﻿using MessagePack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpcTests.MessagePackTest
+{
+	[TestClass]
+	public class MessagePackTest
+	{
+
+
+		[TestMethod]
+		public void TestMessageSerailizeString()
+		{
+			MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+
+			var obj = "hello";
+			var bytes = MessagePack.MessagePackSerializer.Serialize(obj);
+			var obj2 = MessagePack.MessagePackSerializer.Deserialize<string>(bytes);
+			Assert.AreEqual(obj, obj2);
+
+
+		}
+		[TestMethod]
+		public void TestMessageSerailizeInt()
+		{
+			MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+			var obj = 123;
+			var bytes = MessagePack.MessagePackSerializer.Serialize(obj);
+			var obj2 = MessagePack.MessagePackSerializer.Deserialize<int>(bytes);
+			Assert.AreEqual(obj, obj2);
+		}
+
+	}
+	[Serializable]
+	public class TestClass1
+	{
+		public string Name { get; set; } = "hello";
+		public object[] Data { get; set; } = new object[]
+		{
+			new TestClass2(),
+			new TestClass2()
+		};
+	}
+	[Serializable]
+
+	public class TestClass2
+	{
+		public string[] Data = new string[] { "str1", "str2" };
+	}
+
+}
